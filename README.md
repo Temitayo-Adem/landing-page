146fda8ed10ce4d8af658c1508646fe545729dab
Temitayo Ademuwagun <temitayo.adem@gmail.com>
2022-05-31 12:08:39 -0400

    First Attempt
    I've made some adjustments to the code to make the webpage more responsive

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+For this project I tried to keep it fairly simple. I started by adding an additional section to the HTML and linking the javascript file at the bottom of the HTML file.
 
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Note: I did have some difficulty trying to add the active class to a section when it is in the viewport. I found it difficult to determine the perfect moment in which the element is in the viewport, and have currently settled on when the element.getBoundingClientRect.top is greater than zero and the element.getBoundingClientRec.bottom is less than the innerHeight of the window(i.e when the whole vertical aspect of the element is in view). I would like to be a bit more specific than that. Currently, if the screen is sufficiently small and the section is sufficiently large, then there will be no responsive active state. I also think there is a more efficient way to create and call the function that checks if the section is in view. Currently the time complexity is n\*\*2 and I'm calling the function every 100ms regardless of scroll.
--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@
  * Define Global Variables
  * 
 */
-
+// Global variables that represent the unordered list element, an array of the section elements, and the height of the window respectively.
+const navArea = document.getElementById('navbar__list');
+const sections = document.getElementsByTagName('section');
+const height = window.innerHeight;
 
 /**
  * End Global Variables
@@ -39,12 +42,71 @@
 */
 
 // build the nav
+function navBuilder() {
+    // Creating a document fragment that I will append each list item to
+    const docFrag = document.createDocumentFragment();
+    // Iterating through each section element in order to dynamically
+    // add list items (section names) to the navbar.
+    for (const section of sections) {
+        // Using the dataset/id property in order to
+        // get the name of the href link and the name of the section
+        const sectName = section.dataset.nav;
+        const sectId = section.id;
+        const listItem = document.createElement('li');
+        // Giving the list items a horizontal display/appearance
+        listItem.style = "display: inline-block;";
+        // Using a template literal to add innerHTML to list items
+        listItem.innerHTML = `<a href='#${sectId}'>${sectName}</a>`;
+        // Giving list items the css class 'menu__link' as per stylesheet
+        listItem.className = 'menu__link';
+        docFrag.appendChild(listItem);
+    }
+    navArea.appendChild(docFrag);
+}
+
+
 
 
 // Add class 'active' to section when near top of viewport
 
+function activate() {
+    // Iterate through each section element
+    for (let section of sections){
+        // Determine the position of each section
+        // Check if the section is in vertical view
+        let sectionPosition = section.getBoundingClientRect();
+        if ((sectionPosition.top >= 0 && sectionPosition.bottom <= height) || (sectionPosition.top >= -(.20 * sectionPosition.height)) &&  (sectionPosition.top <= (.8 * sectionPosition.height))) {
+            // If the section is in view, remove active class from other sections
+            // Add the active class to the section in view.
+            
+            let winner = section;
+            for (let section of sections) {
+                section.classList.remove('your-active-class');
+            }
+            winner.classList.add('your-active-class')
 
+        }
+
+    }
+
+
+}
 // Scroll to anchor ID using scrollTO event
+function sectScroll(event) {
+    // Prevent the default on-click property (jump-skip)
+    event.preventDefault();
+    // Get the href link and find the element with that Id
+    const link = event.target.getAttribute('href');
+    const toSect = document.querySelector(link);
+    // offsetTop returns the top position of the element relative to it's parents
+    // This property is useful because all of the sections have the same parent
+    // The positioning of any given 'location' is relative to the parent element (main) 
+    const location = toSect.offsetTop;
+    window.scrollTo({
+        top: location,
+        behavior: 'smooth'
+    });
+}
 
 
 /**
@@ -54,9 +116,12 @@
 */
 
 // Build menu 
-
+setTimeout(navBuilder,0);
 // Scroll to section on link click
+navArea.addEventListener('click',sectScroll);
 
 // Set sections as active
+setInterval(activate,100);
+
 
 
