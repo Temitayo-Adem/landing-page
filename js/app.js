4bdbb17d2fe9dcc9ad72fb4f985d07f38b06b14a
Temitayo Ademuwagun <temitayo.adem@gmail.com>
2022-06-02 12:06:35 -0400

    More responsive
    Added function to remove navbar when not scrolling or at top of screen. Made small fixes to make program function more smoothly.

--- js/app.js
+++ js/app.js
@@ -22,19 +22,53 @@
  * Define Global Variables
  * 
 */
-// Global variables that represent the unordered list element, an array of the section elements, and the height of the window respectively.
+// Global variables that represent the unordered list element, a collection of the section elements, the height of the window, the pageHeader where the navbar resides, and a variable (isScrolling) used for timeouts respectively.
 const navArea = document.getElementById('navbar__list');
 const sections = document.getElementsByTagName('section');
+let lastActiveSection = document.querySelector('.your-active-class');
 const height = window.innerHeight;
+const pageHeader = document.querySelector('.page__header');
+let isScrolling;
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+// Function to remove the pageHeader from view when user is
+// not at top of screen and not scrolling
 
+function navSlide(e) {
+    // Check whether the user is at the top of the document.
+    // If so, display the navbar. If not, hide the navbar.
+    if (document.body.scrollTop === 0) {
+        pageHeader.style = "top: 0";
+    } else {
+        pageHeader.style = "top: -50px";
+    }
+}
+function checker(){
+    /* If user is scrolling, stop the setTimeout navSlide function and make sure the navbar is displayed
+    */ 
+    clearTimeout(isScrolling)
+    pageHeader.style = "top: 0";
+    // When user is finished scrolling call the navSlide function
+    isScrolling = setTimeout(navSlide,2000, false);
+}
+/* Event listener that calls the checker function 
+on scroll event*/ 
+document.addEventListener('scroll',checker);
 
-
+// Helper function used to initialize the sectScroll function
+function turnOnSectScroll() {
+    /* This function specifically targets the dynamically generated list items and adds/calls the event listener that
+    creates a smooth scroll.
+    */
+    const menuLinks = document.getElementsByClassName('menu__link');
+    for (let menuLink of menuLinks) {
+        menuLink.addEventListener('click',sectScroll);
+    }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -45,11 +79,10 @@ const height = window.innerHeight;
 function navBuilder() {
     // Creating a document fragment that I will append each list item to
     const docFrag = document.createDocumentFragment();
-    // Iterating through each section element in order to dynamically
-    // add list items (section names) to the navbar.
+    /* Iterating through each section element in order to dynamically add list items (section names) to the navbar.
+    */
     for (const section of sections) {
-        // Using the dataset/id property in order to
-        // get the name of the href link and the name of the section
+        /* Using the dataset/id property to get the name of the href link and the name of the section*/
         const sectName = section.dataset.nav;
         const sectId = section.id;
         const listItem = document.createElement('li');
@@ -70,20 +103,18 @@ function navBuilder() {
 // Add class 'active' to section when near top of viewport
 
 function activate() {
-    // Iterate through each section element
+    // Iterate through html collection of sections
     for (let section of sections){
         // Determine the position of each section
         // Check if the section is in vertical view
         let sectionPosition = section.getBoundingClientRect();
         if ((sectionPosition.top >= 0 && sectionPosition.bottom <= height) || (sectionPosition.top >= -(.20 * sectionPosition.height)) &&  (sectionPosition.top <= (.8 * sectionPosition.height))) {
-            // If the section is in view, remove active class from other sections
+            // If the section is in view, remove active class from previous active section.
             // Add the active class to the section in view.
-            
-            let winner = section;
-            for (let section of sections) {
-                section.classList.remove('your-active-class');
-            }
-            winner.classList.add('your-active-class')
+            lastActiveSection.classList.remove('your-active-class');
+            section.classList.add('your-active-class');
+            lastActiveSection = section;
+
 
         }
 
@@ -118,8 +149,8 @@ function sectScroll(event) {
 // Build menu 
 setTimeout(navBuilder,0);
 // Scroll to section on link click
-navArea.addEventListener('click',sectScroll);
 
+setTimeout(turnOnSectScroll,200);
 // Set sections as active
 setInterval(activate,100);
 
